2df2b190a36e251905b67b79d0f893e343906f4d
fidelis <fidelisnwabisi@gmail.com>
2023-07-26 20:29:14 +0100

    added SharedPreferences and multiprovider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:light_dark_switch/app_theme.dart';
+import 'package:light_dark_switch/constant/constant.dart';
 import 'package:light_dark_switch/home_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+  bool isLightTheme = prefs.getBool(SPref.isLight) ?? true;
+  runApp(AppStart(
+    isLightTheme: isLightTheme,
+  ));
+}
+
+class AppStart extends StatelessWidget {
+  const AppStart({super.key, required this.isLightTheme});
+
+  final bool isLightTheme;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(providers: [
+      ChangeNotifierProvider(
+        create: (_) => ThemeProvider(isLightTheme: isLightTheme),
+      ),
+    ], child: MyApp());
+  }
 }
 
 class MyApp extends StatelessWidget {
